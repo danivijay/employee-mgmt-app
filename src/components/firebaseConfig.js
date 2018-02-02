3084b6f62496199765f0c668eb5bcab12ce63bd3
Dani Vijay <danimvijay@gmail.com>
2018-02-02 21:13:56 +0530

    init webpack, add firebaseConfig, init firebase

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyA3xCUUdmEUkA4ikbpaFjlATg4P4ZDwSCQ',
+  authDomain: 'emp-mgmt-app.firebaseapp.com',
+  databaseURL: 'https://emp-mgmt-app.firebaseio.com',
+  projectId: 'emp-mgmt-app',
+  storageBucket: 'emp-mgmt-app.appspot.com',
+  messagingSenderId: '340462443782'
+}
