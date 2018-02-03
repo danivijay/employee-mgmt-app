b4d503bfee14979a05684cced69e5146b1998088
Dani Vijay <danimvijay@gmail.com>
2018-02-03 16:56:27 +0530

    fetch employees to dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,7 +1,9 @@
 <template>
   <v-layout row wrap>
       <v-flex xs12 sm8 md6 offset-sm2 offset-md3>
-        <v-toolbar color="white" class="mt-2">
+
+        <v-card>
+          <v-toolbar color="white" class="mt-2 elevation-0">
             <v-toolbar-title>Employees</v-toolbar-title>
             <v-btn
               to="/new"
@@ -17,26 +19,61 @@
             </v-btn>
           </v-toolbar>
 
+          <v-divider></v-divider>
+
           <v-list two-line>
-            <v-list-tile to='/view/1'>
+            <v-list-tile
+              v-for="employee in employees"
+              :to="{
+                name: 'view-employee',
+                params: {
+                    employee_id: employee.employee_id
+                  }
+                }"
+              :key="employee.id">
+
               <v-list-tile-content>
                 <v-list-tile-title>
-                  Hello
+                  {{ employee.employee_id }}. {{ employee.name }}
                 </v-list-tile-title>
+
                 <v-list-tile-sub-title>
-                  World
+                  <v-chip>{{ employee.dept }}</v-chip> {{ employee.position }}
                 </v-list-tile-sub-title>
               </v-list-tile-content>
+
             </v-list-tile>
           </v-list>
+        </v-card>
 
       </v-flex>
   </v-layout>
 </template>
 
 <script>
-export default {
+import db from './firebaseinit'
 
+export default {
+  data () {
+    return {
+      employees: []
+    }
+  },
+  created () {
+    db.collection('employees').get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          const data = {
+            'id': doc.id,
+            'employee_id': doc.data().employee_id,
+            'name': doc.data().name,
+            'dept': doc.data().dept,
+            'position': doc.data().position
+          }
+          this.employees.push(data)
+        })
+      })
+  }
 }
 </script>
 
